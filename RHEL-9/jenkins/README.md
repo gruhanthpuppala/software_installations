1aa5730ae0d15cd134e91357334108e38e919481
gruhanthpuppala <gruhanth.puppala@gmail.com>
2025-05-17 19:23:07 +0530

    added jenkins_installation in rhel

--- RHEL-9/jenkins/README.md
+++ RHEL-9/jenkins/README.md
@@ -1,1 +1,2 @@
-#need to update 
+This script is for --> Red Hat Enterprise Linux version 9 image
+This script consists of java-17 installation and adding jenkins repo to rhel repo and install jenkins.
--- RHEL-9/jenkins/jenkins_installation.sh
+++ RHEL-9/jenkins/jenkins_installation.sh
@@ -1,1 +1,9 @@
-#need to update
+sudo curl --silent --location --output /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install -y java-17-openjdk
+sudo yum install -y jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
